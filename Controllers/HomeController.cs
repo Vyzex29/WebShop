070feaf7bbb25088f211f1f4003fa3a3638e0269
Vyzex29 <virtuoso292@gmail.com>
2022-06-20 19:44:56 +0300

    Added a different design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebShop.Context;
 using WebShop.Models;
 
 namespace WebShop.Controllers
@@ -7,15 +8,48 @@ namespace WebShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly WebShopDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WebShopDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? selectedCategory = 1)
         {
-            return View();
+
+            List<CategoryModel> categories = new List<CategoryModel>();
+            List<ProductModel> products = new List<ProductModel>();
+            using (_context)
+            {
+                categories = _context.Categories.Select(categoryFromDb => new CategoryModel
+                {
+                    Id = categoryFromDb.Id,
+                    Name = categoryFromDb.Name
+                }).ToList();
+
+                if (categories.Exists(c=>c.Id == selectedCategory))
+                {
+                    products = _context.Products.Where(pr => pr.Category.Id == selectedCategory).Select(productFromDb => new ProductModel
+                    {
+                        Id = productFromDb.Id,
+                        Name = productFromDb.Name,
+                        Description = productFromDb.Description,
+                        Price = productFromDb.Price
+                    }).OrderBy(product => product.Name)
+                .ToList();
+                }
+                
+            }
+
+            var homeView = new HomeViewModel
+            {
+                categories = categories,
+                products = products,
+            };
+
+            return View(homeView);
         }
 
         public IActionResult Privacy()
---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebShop.Models
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+            categories = new List<CategoryModel>();
+            products = new List<ProductModel>();
+        }
+
+        public List<CategoryModel> categories;
+
+        public List<ProductModel> products;
+    }
+}
