402f94c146e1320b0de2330a36b03807cdbec92c
Vyzex29 <virtuoso292@gmail.com>
2022-07-14 19:40:13 +0300

    Add identity project as playground

---
+++ UserTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
