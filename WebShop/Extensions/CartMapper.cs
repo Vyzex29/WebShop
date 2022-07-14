677b6f499ccf4cc1079dcbf77478af7a86962a56
Vyzex29 <virtuoso292@gmail.com>
2022-07-14 19:56:27 +0300

    Add cartMapper as comparison to extension

---
+++ WebShop/Extensions/CartMapper.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Context.Entity;
+using WebShop.Models;
+
+namespace WebShop.Extensions
+{
+    public class CartMapper
+    {
+        public  CartModel ToModel(Cart cart)
+        {
+            List<CartItemModel> cartItems = new List<CartItemModel>();
+            CartModel cartModel = new CartModel
+            {
+                Id = cart.Id,
+                CartItems = cartItems
+            };
+
+            return cartModel;
+        }
+    }
+}
